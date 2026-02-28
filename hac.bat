81d7ff2541cfe114032bc010623773a6a42c3631
IGOR10S <igorsamotoy29@gmail.com>
2026-02-28 23:46:38 +0100

    Script

--- hac.bat
+++ hac.bat
@@ -3,27 +3,26 @@ setlocal enabledelayedexpansion
 
 :: Check if a file with IP addresses has been passed
 if "%~1"=="" (
-    echo Usage: .\%~nx0 .\name_file.txt
-    echo NOTE: The file must contain one IP/Hostname per line.
+    echo Usage: .\%~nx0 .\file_name.txt
+    echo The input txt file must contain one IP/Hostname per line
     exit /b
 )
 
 :: Check if the file exists
 if not exist "%~1" (
-    echo The file %~1 does not exist.
+    echo The file '%~1' does not exist
     exit /b
 )
 
 :: Create or empty the report file
 > hac_report.txt echo Ping results:
-echo ---------------------------------- >> hac_report.txt
 
 set count=1
 
 :: Read IP addresses from file and ping them one by one
 for /f "usebackq tokens=*" %%I in ("%~1") do (
-    echo [!count!] Pinging "%%I" ...
-    ping "%%I" -n 4 -w 2000 > nul
+    echo [!count!] Pinging %%I ...
+    ping "%%I" -n 1 -w 2000 > nul
     if errorlevel 1 (
         echo     UNREACHABLE
         echo UNREACHABLE - %%I>> hac_report.txt
@@ -31,9 +30,8 @@ for /f "usebackq tokens=*" %%I in ("%~1") do (
         echo     REACHABLE
         echo REACHABLE - %%I>> hac_report.txt
     )
-    echo -------------------------
+    echo(
     set /a count+=1
 )
 
 echo Report saved in hac_report.txt
-pause
